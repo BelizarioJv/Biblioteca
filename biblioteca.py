8b254319d5e3ef5bfc0bef22906a8a9e82ae4aac
João victor Belizario dos santos <113644605+BelizarioJv@users.noreply.github.com>
2025-07-28 22:33:12 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,38 +1,44 @@
+
+#Você foi contratado para desenvolver um sistema simples de cadastro de livros para uma biblioteca.
+#O programa deve permitir que o usuário registre informações sobre vários livros, realizando validações e armazenando dados importantes.
 totalLivros = 0
-totalNovo = 0
-MaisAntigo = ''
-MaisAntigo_Ano = 0 
-totalUsados = 0
-
-
+livrosUsados = 0
+livrosNovos = 0
+idadeMaisAntigo = 0
+maisAntigo = ''
 while True:
-    nomeLivro = input('Digite o nome do livro').strip()
-    anoPublicaçao_str= input('Digite o ano da publicaçao do livro')
-    if anoPublicaçao_str.isdigit() and anoPublicaçao_str != 0:
-        anoPublicaçao = int(anoPublicaçao_str)
-    else:
-        print('Digito invalido')
-        break
-    
-    estadoLivro = input('Digite o estado fisico do livro:NOVO ou USADO').strip().upper()
-    if estadoLivro == 'NOVO':
-        totalNovo += 1
-    elif estadoLivro =='USADO':
-        MaisAntigo = nomeLivro
-        MaisAntigo_Ano = anoPublicaçao
-        
-    totalLivros +=1
-    continuar = input('Deseja cadastrar mais um aluno e as suas notas?(S/N)').strip().upper()
-    if continuar != 'S':
-     break
-    
-    
-    
-print('***********************************************')
-print('-                                                  -')
-print(f'A biblioteca tem {totalNovo} livros novos')
-print(f'O total de livros na biblioteca e de {totalLivros}')
-if totalUsados != 0:   
- print(f'O mais antigo e o {MaisAntigo} que foi publicado em {MaisAntigo_Ano}')
-print('-                                                  -')
-print('***********************************************')
+  nomeLivro = input('Digite o nome do livro').strip()
+  anoLançamento_str = input('Digite o ano de lançamento do livro')
+  if anoLançamento_str.isdigit():
+    anoLançamento = int(anoLançamento_str)
+  else:
+    print('Digito invalido')
+  
+  totalLivros += 1
+
+  estadoLivro = input('Qual o estado do livro:NOVO ou USADO: ').upper().strip()
+  if estadoLivro == 'NOVO':
+    livrosNovos += 1
+  elif estadoLivro == 'USADO':
+      livrosUsados += 1
+  else:
+    print('Digito invalido')
+
+  if anoLançamento > idadeMaisAntigo :
+    maisAntigo += nomeLivro
+    idadeMaisAntigo += anoLançamento
+  
+  continuar = input('Deseja cadastrar outro veiculo?(S/N): ').strip().upper()
+  if continuar == 'S':
+    continue
+  elif continuar == 'N':
+   print('****************DADOS CADASTRADOS********************')
+   print('-                                                       -')
+   print(f'O total de livros cadastrados é:{totalLivros}, sendo eles {livrosNovos} livros novos e {livrosUsados} livros usados.\n ')
+   print(f'O livro mais antigo e o {maisAntigo} que foi publicado em {idadeMaisAntigo}\n')
+  else:
+    print('Digito incorreto')
+    break
+  
+
+   
